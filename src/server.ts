708f76b06c508ce91df5247de1bf72f1d3fcf2a5
rodolfossilvadev <rodolfodev30@gmail.com>
2025-07-17 11:45:10 -0300

    fix: usar porta do ambiente e host para render

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,22 @@
 import app from "./app";
 import { prismaConnect } from "./config/prisma";
 import { initializeGlobalCategories } from "./services/globalCategories.service";
-import { env } from "./config/env";
 import initializeFirebaseAdmin from "./config/firebase";
 
-const PORT = env.PORT;
 initializeFirebaseAdmin();
 
 const startServer = async () => {
-    try {
+  try {
+    await prismaConnect();
+    await initializeGlobalCategories();
 
-        await prismaConnect();
+    const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
 
-        await initializeGlobalCategories();
+    await app.listen({ port: PORT, host: "0.0.0.0" });
+    console.log(`Servidor rodando na porta ${PORT}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-        await app.listen({ port: PORT }).then(() => {
-            console.log(`Servidor rodando na porta ${PORT}`)
-        })
-    } catch (err) {
-        console.error(err)
-    }
-}
-
-startServer()
+startServer();
