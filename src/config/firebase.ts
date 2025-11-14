df2dafe5feebad64d92abeddad85cafca196734d
rodolfossilvadev <rodolfodev30@gmail.com>
2025-11-14 10:42:48 -0300

    log corretos

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,10 +2,6 @@ import admin from "firebase-admin";
 import { env } from "./env";
 
 const initializeFirebaseAdmin = (): void => {
-  console.log("FIREBASE_CLIENT_EMAIL:", JSON.stringify(FIREBASE_CLIENT_EMAIL));
-console.log("FIREBASE_PROJECT_ID:", JSON.stringify(FIREBASE_PROJECT_ID));
-console.log("FIREBASE_PRIVATE_KEY LENGTH:", FIREBASE_PRIVATE_KEY?.length);
-
   if (admin.apps.length > 0) return;
 
   const {
@@ -14,8 +10,13 @@ console.log("FIREBASE_PRIVATE_KEY LENGTH:", FIREBASE_PRIVATE_KEY?.length);
     FIREBASE_PROJECT_ID,
   } = env;
 
+  // Logs após carregar as envs
+  console.log("FIREBASE_CLIENT_EMAIL:", FIREBASE_CLIENT_EMAIL ? "***OK***" : "***MISSING***");
+  console.log("FIREBASE_PROJECT_ID:", FIREBASE_PROJECT_ID ? "***OK***" : "***MISSING***");
+  console.log("FIREBASE_PRIVATE_KEY LENGTH:", FIREBASE_PRIVATE_KEY?.length || 0);
+
   if (!FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY || !FIREBASE_PROJECT_ID) {
-    throw new Error("Falha ao iniciar Firebase - Faltando as credenciais");
+    throw new Error("Falha ao iniciar Firebase - Faltando credenciais");
   }
 
   try {
@@ -23,9 +24,10 @@ console.log("FIREBASE_PRIVATE_KEY LENGTH:", FIREBASE_PRIVATE_KEY?.length);
       credential: admin.credential.cert({
         projectId: FIREBASE_PROJECT_ID,
         clientEmail: FIREBASE_CLIENT_EMAIL,
-        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), 
+        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       }),
     });
+    console.log("Firebase inicializado com sucesso 🚀");
   } catch (err) {
     console.error("Falha ao conectar Firebase!", err);
     process.exit(1);
